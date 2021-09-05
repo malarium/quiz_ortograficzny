a66f31141723b9c19d0b25761f544a33f81049e7
Marcin <marcingorecki1981@gmail.com>
2021-09-05 23:51:26 +0200

    Landscape orientation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App(): JSX.Element {
+  React.useEffect(() => {
+    lockLandscapeOrientation();
+  }, []);
+
+  async function lockLandscapeOrientation() {
+    await ScreenOrientation.lockAsync(
+      ScreenOrientation.OrientationLock.LANDSCAPE
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Okay, it seems to work now</Text>
       <StatusBar style="auto" />
     </View>
   );
